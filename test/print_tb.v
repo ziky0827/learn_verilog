9df48dbd44efb20f07da5f4b4108eefd8dd1ac95
ziky0827 <79458297@qq.com>
2023-10-25 14:56:50 +0800

    去掉波形注释

--- test/print_tb.v
+++ test/print_tb.v
@@ -1,11 +1,4 @@
 //将输出打印
-    { signal: [
-        { name: "clk",         wave: "p.....|..." },
-        { name: "Data",        wave: "x.345x|=.x", data: ["head", "body", "tail", "data"] },
-        { name: "Request",     wave: "0.1..0|1.0" },
-        {},
-        { name: "Acknowledge", wave: "1.....|01." }
-    ]}
 
 module print_tb();
 
